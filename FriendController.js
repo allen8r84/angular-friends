aab7b48df3e8b009acaafedba52c71d7c04a7546
Jon <jon@allen8r.com>
2015-02-09 20:16:45 -0700

    work on step two - at this point I cannot properly reference the .json file - I am at a loss and need a break to clear my thoughts and get back on it later

--- FriendController.js
+++ FriendController.js
@@ -2,7 +2,11 @@ var app = angular.module("myFriends");
 
 app.controller("MainController", function($scope) {
     $scope.test = "Hello World";
-    
+    $scope.friendz = [];
+    $.getJSON('friend-data.json', function(res) {
+        $scope.friendz = res;
+        console.log(friendz);
+    });
     
     
 });
